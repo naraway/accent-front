3042623119ab0a85d654ff008c62f001b5065116
Seungbong Lee <mervonee@gmail.com>
2023-05-25 18:23:30 +0900

    chore: Add props in stage request

--- src/context/StageRequest.ts
+++ src/context/StageRequest.ts
@@ -8,6 +8,10 @@ export interface StageRequest {
   enabled?: boolean;
   actorId?: string;
   pavilionId?: string;
+  osid?: string;
+  usid?: string;
+  kollectionId?: string;
+  dramaId?: string;
   cineroomIds?: string[];
   roles?: string[];
   attributes?: { [key: string]: string[] };
